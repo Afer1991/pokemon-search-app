dbb1cdf174aac1e782cc1e2adee3a013f220cdb9
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-06-08 16:18:53 -0400

    Add function to display pokemon stats

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ const pokeDefense = document.getElementById("defense");
 const pokeSpAttack = document.getElementById("special-attack");
 const pokeSpDefense = document.getElementById("special-defense");
 const pokeSpeed = document.getElementById("speed");
+
+
+const returnPokeStats = (pokemon) => {
+  fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemon}`)
+  .then((res) => res.json())
+  .then((data) => {
+    pokeName.innerText = data.name.toUpperCase();
+    pokeId.innerText = `#${data.id}`;
+    pokeWgt.innerText = `Weight: ${data.weight}`;
+    pokeHgt.innerText = `Height: ${data.height}`;
+    imgContainer.innerHTML = `<img src="${data.sprites.front_default}" alt="${data.name}">`;
+    pokeTypes.innerHTML = `<div class="type ${data.types[0].type.name}">${data.types[0].type.name}</div>
+                           ${data.types[1] != undefined ? `<div class=\"type ${data.types[1].type.name}\">${data.types[1].type.name}</div>`
+                                                        : ""}`;
+    pokeHp.innerText = data.stats[0].base_stat;
+    pokeAttack.innerText = data.stats[1].base_stat;
+    pokeDefense.innerText = data.stats[2].base_stat;
+    pokeSpAttack.innerText = data.stats[3].base_stat;
+    pokeSpDefense.innerText = data.stats[4].base_stat;
+    pokeSpeed.innerText = data.stats[5].base_stat;                    
+});  
+};
